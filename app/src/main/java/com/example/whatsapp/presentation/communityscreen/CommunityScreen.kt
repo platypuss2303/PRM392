cb5cbba822547984f443dcab3767dccc27c53e6f
Nguyễn Gia Luật <nguyengialuatace@gmail.com>
2025-09-22 12:35:45 +0700

    Community Screen

--- app/src/main/java/com/example/whatsapp/presentation/communityscreen/CommunityScreen.kt
+++ app/src/main/java/com/example/whatsapp/presentation/communityscreen/CommunityScreen.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -30,4 +32,36 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.whatsapp.presentation.bottomnavigation.BottomNavigation
+
+@Composable
+@Preview(showSystemUi = true)
+fun CommunityScreen(){
+    val sampleCommunities = listOf(
+        Communities(image = R.drawable.neat_roots, name = "Neat Roots", description = "We love plants"),
+        Communities(image = R.drawable.neat_roots, name = "Neat Roots", description = "We love plants"),
+        Communities(image = R.drawable.neat_roots, name = "Neat Roots", description = "We love plants"),
+    )
+    Scaffold(
+        topBar = {TopBar()},
+        bottomBar = {BottomNavigation()}
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+            Button(onClick = {}, colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.light_green)),
+                modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+                Text(text = "Start a new Community", fontSize = 16.sp, fontWeight = FontWeight.Bold, color = Color.White)
+                Spacer(modifier = Modifier.height(8.dp))
+
+            }
+            Text(text = "Your Communities", fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color.Black,
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp))
+
+            LazyColumn {
+                items(sampleCommunities){
+                    CommunityItemDesign(communities = it)
+                }
+            }
+
+        }
+    }
+}
 
