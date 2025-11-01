287fce5149bdb0cf9d1e9075b08b1a52ac20e4a0
Nguyễn Gia Luật <nguyengialuatace@gmail.com>
2025-11-01 22:17:49 +0700

    UI profile

--- instagram/build.gradle.kts
+++ instagram/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation("com.google.android.material:material:1.12.0")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
@@ -59,4 +60,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
 }
--- instagram/src/main/java/com/example/instagram/MainActivity.kt
+++ instagram/src/main/java/com/example/instagram/MainActivity.kt
@@ -1,52 +1,63 @@
 package com.example.instagram
 
 import android.os.Bundle
-import android.widget.TextView
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.instagram.R
 import com.example.instagram.fragments.HomeFragment
 import com.example.instagram.fragments.NotificationsFragment
 import com.example.instagram.fragments.ProfileFragment
 import com.example.instagram.fragments.SearchFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-
 
 
 class MainActivity : AppCompatActivity() {
-    internal var selectedFragment: Fragment? = null
+
+
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.nav_home -> {
-                selectedFragment = HomeFragment()
+                moveToFragment(HomeFragment())
+                return@OnNavigationItemSelectedListener true
             }
             R.id.nav_search -> {
-                selectedFragment = SearchFragment()
+                moveToFragment(SearchFragment())
+                return@OnNavigationItemSelectedListener true
             }
             R.id.nav_add_post -> {
+                return@OnNavigationItemSelectedListener true
             }
             R.id.nav_notification -> {
-                selectedFragment = NotificationsFragment()
+                moveToFragment(NotificationsFragment())
+                return@OnNavigationItemSelectedListener true
             }
             R.id.nav_profile -> {
-                selectedFragment = ProfileFragment()
+                moveToFragment(ProfileFragment())
+                return@OnNavigationItemSelectedListener true
             }
         }
-    if(selectedFragment != null){
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
-        selectedFragment!!
-    }
+
         false
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+
+        moveToFragment(HomeFragment())
+    }
+
+
+    private fun moveToFragment(fragment: Fragment)
+    {
+        val fragmentTrans = supportFragmentManager.beginTransaction()
+        fragmentTrans.replace(R.id.fragment_container, fragment)
+        fragmentTrans.commit()
     }
 }
-
